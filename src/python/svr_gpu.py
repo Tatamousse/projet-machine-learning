60e0a385fcbc10f39738d619a435dcfc75193249
Tatamousse <hayteca@gmail.com>
2026-02-23 15:27:30 +0100

    changé la sample size dudu svr

--- src/python/svr_gpu.py
+++ src/python/svr_gpu.py
@@ -4,14 +4,18 @@ import pandas as pd
 from cuml.svm import SVR
 from cuml.preprocessing import StandardScaler
 
-# 1. Chargement des données
-train = pd.read_csv("../../data/train_processed.csv")
+#Chargement des données
+train_full = pd.read_csv("../../data/train_processed.csv")
+train= train_full.sample(n=100000, random_state=42)
+
 test  = pd.read_csv("../../data/test_processed.csv")
+
+
 
 print("Train shape:", train.shape)
 print("Test shape :", test.shape)
 
-# 2. One-Hot Encoding sécurisé (toutes les colonnes catégorielles deviennent numériques)
+#One-Hot Encoding sécurisé (toutes les colonnes catégorielles deviennent numériques)
 full = pd.concat([train.drop(columns=["exam_score"]), test], axis=0, ignore_index=True)
 full_encoded = pd.get_dummies(full, drop_first=True)
 
@@ -19,12 +23,12 @@ X = full_encoded.iloc[:len(train)]
 X_test = full_encoded.iloc[len(train):]
 y = train["exam_score"]
 
-# 3. Standardisation (OBLIGATOIRE pour SVR GPU)
+#Standardisation (OBLIGATOIRE pour SVR GPU)
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X.values.astype(np.float32))
 X_test_scaled = scaler.transform(X_test.values.astype(np.float32))
 
-# 4. Entraînement final SVR GPU avec les meilleurs paramètres
+#Entraînement final SVR GPU avec les meilleurs paramètres
 final_model = SVR(
     kernel="rbf",
     C=3,
@@ -33,7 +37,7 @@ final_model = SVR(
 )
 final_model.fit(X_scaled, y)
 
-# 5. Prédiction test + export Kaggle
+#Prédiction test + export Kaggle
 test_preds = final_model.predict(X_test_scaled)
 test_preds = np.clip(test_preds, 0, 100)
 
